2e2f78b3cf726124e8e5908f8b3c1165e3934dfa
MigstDevs <battlebloonguy@gmail.com>
2024-10-03 20:53:06 -0300

    Changed the freedoms messages for the interaction command (text command soon enough).

--- commands/freedoms.js
+++ commands/freedoms.js
@@ -6,7 +6,20 @@ async function comandoFreedomsExecutar (interaction, options) {
   const chosen_user = options.getUser("user") || interaction.user;
   const userId = chosen_user.id;
   const userFreedoms = freedoms[userId] || 0;
-  await interaction.editReply(`💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms! A _liberdade_ está perto! 🪙`);
+
+  if (userFreedoms <= 0) {
+    return await interaction.editReply(`💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 😭 **|** Em pobreza, só perde pra mim!`);
+  } else if (userFreedoms >= 10000 && userFreedoms < 50000) {
+    return await interaction.editReply(`💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 💰 **|** É um salário mínimo, e portanto, pode melhorar, mas é melhor do que nada!`);
+  } else if (userFreedoms >= 50000 & userFreedoms < 100000) {
+    return await interaction.editReply(`💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 🤑 **|** Já tá bom, mas continue com seus sonhos! (Cadê a <@297153970613387264> kkkk)`);
+  } else if (userFreedoms >= 100000 & userFreedoms < 200000 ) {
+    return await interaction.editReply(`💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 🫢 **|** Uau! Você tá incrível, continua assim!`);
+  } else if (userFreedoms >= 200000 && userFreedoms < 500000) {
+    return await interaction.editReply(`💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 💵 **|** Nossa! Você tá rico!`);
+  } else if (userFreedoms >= 500000) {
+    return await interaction.editReply(`💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 🪙 **|** A _liberdade_ está perto!`);
+  }
 }
 
 export { comandoFreedomsExecutar };
