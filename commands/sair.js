2fd9fd4f9a4e1fd5cb8611b22e7a1e489044cdba
MigstDevs <battlebloonguy@gmail.com>
2024-10-03 16:41:09 -0300

    Updated the leave command.

--- commands/sair.js
+++ commands/sair.js
@@ -1,22 +1,24 @@
 async function comandoSairExecutar(interaction) {
     const user = interaction.user;
-    const member = interaction.guild.members.cache.get(user.id);
+    const member = interaction.guild.members.cache.get(user.id); 
     const owner = await interaction.guild.fetchOwner();
-    const botMember = interaction.guild.members.cache.get(interaction.client.user.id);
+    const botMember = interaction.guild.members.cache.get(interaction.client.user.id); 
+
+    await interaction.deferReply();
 
     if (user.id === owner.id) {
-        return await interaction.reply(`🫤 **|** Cara... O que cê tá pensando?\n👑 **|** Você, <@${owner.id}>, é o **DONO** deste servidor. Donos do próprio servidor não podem ~~ser expulsos~~ sair do servidor!`);
+        return await interaction.editReply(`🫤 **|** Cara... O que cê tá pensando?\n👑 **|** Você, <@${owner.id}>, é o **DONO** deste servidor. Donos do próprio servidor não podem ~~ser expulsos~~ sair do servidor!`);
     }
 
     if (!interaction.guild.me.permissions.has('KICK_MEMBERS')) {
-        return await interaction.reply(`🚫 **|** Eu não tenho permissão para ~~expulsar membros~~ te tirar deste servidor.`);
+        return await interaction.editReply(`🚫 **|** Eu não tenho permissão para expulsar membros neste servidor.`);
     }
 
     if (botMember.roles.highest.position <= member.roles.highest.position) {
-        return await interaction.reply(`😬 **|** Eu não posso ~~te expulsar~~ fazer você sair do servidor, <@${user.id}>, porque meu cargo é menor ou igual ao seu na hierarquia de cargos!`);
+        return await interaction.editReply(`😬 **|** Eu não posso expulsar <@${user.id}> porque meu cargo é menor ou igual ao dele na hierarquia de cargos!`);
     }
 
-    await interaction.reply(`💔 **|** Woosh! E lá se vai ele...\n😭 **|** <@${user.id}> saiu do servidor!`);
+    await interaction.editReply(`💔 **|** Woosh! E lá se vai ele...\n😭 **|** <@${user.id}> saiu do servidor!`);
     await member.kick("Quis sair do servidor 😢");
 }
 
