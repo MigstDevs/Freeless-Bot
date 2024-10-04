e624e5c83871d20f7e1fd9a646ce38aa02bc1a78
MigstDevs <battlebloonguy@gmail.com>
2024-10-04 15:29:56 -0300

    New change to the "freedoms" text command.

--- freelessPrincipal.js
+++ freelessPrincipal.js
@@ -138,7 +138,23 @@ client.on("messageCreate", async (message) => {
     const userId = message.author.id;
 
     const userFreedoms = freedoms[userId] || 0;
-    const response = `💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128>freedoms! A _liberdade_ está perto! 🪙`;
+
+    let response = `💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128>freedoms! A _liberdade_ está perto! 🪙`;
+
+    if (userFreedoms <= 0) {
+      response = `💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 😭 **|** Em pobreza, só perde pra mim!`;
+    } else if (userFreedoms >= 10000 && userFreedoms < 50000) {
+      response = `💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 💰 **|** É um salário mínimo, e portanto, pode melhorar, mas é melhor do que nada!`;
+    } else if (userFreedoms >= 50000 & userFreedoms < 100000) {
+      response = `💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 🤑 **|** Já tá bom, mas continue com seus sonhos! (Cadê a <@297153970613387264> kkkk)`;
+    } else if (userFreedoms >= 100000 & userFreedoms < 200000 ) {
+      response = `💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 🫢 **|** Uau! Você tá incrível, continua assim!`;
+    } else if (userFreedoms >= 200000 && userFreedoms < 500000) {
+      response = `💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 💵 **|** Nossa! Você tá rico!`;
+    } else if (userFreedoms >= 500000) {
+      response = `💸 **|** Você possui <:freedoms:1282757761406468128> ${userFreedoms} <:freedoms:1282757761406468128> freedoms!\n 🪙 **|** A _liberdade_ está perto!`;
+    }
+
     message.reply(response);
   }
 
